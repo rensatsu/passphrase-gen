a3a9cb0e1e995f3024124a15e0ebb5804e3e85a1
Pavel Tvaladze <reddragonsportal@gmail.com>
2021-10-02 00:39:16 +0300

    Updated logo (now using SVG)

--- src/components/header.vue
+++ src/components/header.vue
@@ -25,7 +25,7 @@ h1 > a {
 .app-icon {
   width: 48px;
   height: 48px;
-  background: url("../assets/images/icon48.png");
+  background: url("../assets/images/icon.svg");
   background-size: 100%;
   margin: 0 1rem 0 0;
 }
