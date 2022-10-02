c9d1d0b4cc1b1c29dae1795cc59c755490c7f85e
Pavel Tvaladze <reddragonsportal@gmail.com>
2022-10-02 16:31:12 +0400

    Optimize vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,8 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    polyfillDynamicImport: false,
-    target: "es2020",
+  optimizeDeps: {
+    exclude: ["oh-vue-icons/icons"],
   },
   server: {
     proxy: {
